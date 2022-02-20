789ab989cf6b57299aebcb554b2c3c0bd642a4d7
Jaehun Park <50619560+bomsbro@users.noreply.github.com>
2022-02-20 19:38:40 +0900

    Create init-vm-api.sh

---
+++ init-vm-api.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#update before install
+apt-get update
+
+#add docker repository
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -arch
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+
+#docker install
+apt-get install docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+service docker start
+
+#portainer run with auto image pull
+mkdir -p /data/portainer
+docker run --name portainer -p 8091:9000 -d --restart always -v /data/portainer:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+
+#check docker status
+service docker status
