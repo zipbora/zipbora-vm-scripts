6f06f807b71d2060f500e09e5192a3913f847291
Jaehun Park <50619560+bomsbro@users.noreply.github.com>
2022-02-21 14:43:06 +0900

    Update init-vm-storage.sh

--- init-vm-storage.sh
+++ init-vm-storage.sh
@@ -16,7 +16,7 @@ service docker start
 
 #portainer run with auto image pull
 mkdir -p /data/portainer
-docker run --name portainer -p 8093:9000 -d --restart always -v /data/portainer:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
+docker run -d --name portainer -p 8093:9000 --restart always -v /boot/data/portainer:/data -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer
 
 #check docker status
 service docker status
