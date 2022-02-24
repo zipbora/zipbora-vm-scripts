ac912813ae4c500905e5f85e7bc4dd4adf1f624b
Jaehun Park <50619560+bomsbro@users.noreply.github.com>
2022-02-24 18:01:59 +0900

    Update init-vm-api.sh

--- init-vm-api.sh
+++ init-vm-api.sh
@@ -3,15 +3,19 @@
 #update before install
 apt-get update
 
-#add docker repository
+#install docker
 apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -arch
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
-
-#docker install
 apt-get install docker-ce docker-ce-cli containerd.io
 systemctl enable docker
+
+#install docker-compose
+curl -L <https://github.com/docker/compose/releases/download/{설치버전}/docker-compose-`uname> -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#docker start
 service docker start
 
 #portainer run
